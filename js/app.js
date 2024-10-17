f61a508e382bc66b668a8fc6ba7c6a13101a5cff
unknown <alex.chinchi02@gmail.com>
2024-10-17 16:57:34 +0200

    primera parte

--- js/app.js
+++ js/app.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-     
+    // Variables
+    let iconMostrar = document.querySelector(".menu > button");
+    let iconCerrar = document.querySelector(".boton");
+    let navbar = document.querySelector(".navbar");
+
+    // Eventos
+    iconMostrar.addEventListener("click", () => {
+
+        navbar.classList.add("mostrar");
+        setTimeout(() => {
+            navbar.style.right = "0%";
+            navbar.classList.remove("mostrar");
+        }, 999);
+
+    });
+
+    iconCerrar.addEventListener("click", () => {
+
+        navbar.classList.add("ocultar");
+        setTimeout(() => {
+            navbar.style.right = "-20%";
+            navbar.classList.remove("ocultar");
+        }, 1000);
+        
+    });
+
+    navbar.addEventListener("mouse")
 })
